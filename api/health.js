67517b77e2c6d6cdbd271691d0c70d4680e736c6
Waseque1000 <waseque.ndc@gmail.com>
2025-06-09 17:15:53 +0600

    ok

---
+++ api/health.js
@@ -0,0 +1,7 @@
+// module.exports.handler = (req, res) => {
+//   res.json({
+//     status: "OK",
+//     message: "Instagram Full-Resolution Image Extractor API",
+//     version: "1.0.0",
+//   });
+// };
